50bc48157f76df81485e1842bad5a3047fb14eff
Devan Kestel <devan.kestel@gmail.com>
2022-08-06 18:45:08 -0400

    (refactor): remove debug log statements from joining channel.

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -79,8 +79,6 @@ if (chatRoomTitle) {
   })
 
   channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
-    .receive("error", resp => { console.log("Unable to join", resp) })
 }
 
 export default socket
